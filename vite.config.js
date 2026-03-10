8ea366a9be41ca5de5c4898ef50f496ff529c6e6
abdullahAl-Houssein <93677750+abdullahAl-Houssein@users.noreply.github.com>
2026-03-10 15:21:49 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   preview: {
-    allowedHosts: ['telecomsy-manager.onrender.com', 'localhost', '.onrender.com']
+    allowedHosts: 'all',
+  },
+  server: {
+    allowedHosts: 'all',
+    proxy: {
+      '/api': 'http://localhost:3001'
+    }
   }
 })
